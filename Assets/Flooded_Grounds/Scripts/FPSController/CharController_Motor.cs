33d68e2094a058dafd60bb5a65690db6bfc9764d
DavidS12321 <dshahi333@gmail.com>
2024-10-11 01:38:54 -0700

    Added flashlight functionality. Work in progress

--- Assets/Flooded_Grounds/Scripts/FPSController/CharController_Motor.cs
+++ Assets/Flooded_Grounds/Scripts/FPSController/CharController_Motor.cs
@@ -2,70 +2,83 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CharController_Motor : MonoBehaviour {
+public class CharController_Motor : MonoBehaviour
+{
+    public float speed = 10.0f;
+    public float sensitivity = 30.0f;
+    public float WaterHeight = 15.5f;
+    CharacterController character;
+    public GameObject cam;
+    float moveFB, moveLR;
+    float rotX, rotY;
+    float xRotation = 0f; // Variable to store the clamped vertical rotation
+    public bool webGLRightClickRotation = true;
+    float gravity = -9.8f;
 
-	public float speed = 10.0f;
-	public float sensitivity = 30.0f;
-	public float WaterHeight = 15.5f;
-	CharacterController character;
-	public GameObject cam;
-	float moveFB, moveLR;
-	float rotX, rotY;
-	public bool webGLRightClickRotation = true;
-	float gravity = -9.8f;
+    void Start()
+    {
+        // Lock the cursor
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
 
-	void Start(){
-		//LockCursor ();
-		Cursor.visible = false;
-		Cursor.lockState = CursorLockMode.Locked;
-		
-		character = GetComponent<CharacterController> ();
-		if (Application.isEditor) {
-			webGLRightClickRotation = false;
-			sensitivity = sensitivity * 1.5f;
-		}
-	}
+        character = GetComponent<CharacterController>();
+        if (Application.isEditor)
+        {
+            webGLRightClickRotation = false;
+            sensitivity = sensitivity * 1.5f;
+        }
+    }
 
-	void CheckForWaterHeight(){
-		if (transform.position.y < WaterHeight) {
-			gravity = 0f;			
-		} else {
-			gravity = -9.8f;
-		}
-	}
+    void CheckForWaterHeight()
+    {
+        if (transform.position.y < WaterHeight)
+        {
+            gravity = 0f;
+        }
+        else
+        {
+            gravity = -9.8f;
+        }
+    }
 
-	void Update()
-	{
+    void Update()
+    {
+        moveFB = Input.GetAxis("Horizontal") * speed;
+        moveLR = Input.GetAxis("Vertical") * speed;
 
-		moveFB = Input.GetAxis ("Horizontal") * speed;
-		moveLR = Input.GetAxis ("Vertical") * speed;
+        rotX = Input.GetAxis("Mouse X") * sensitivity;
+        rotY = Input.GetAxis("Mouse Y") * sensitivity;
 
-		rotX = Input.GetAxis ("Mouse X") * sensitivity;
-		rotY = Input.GetAxis ("Mouse Y") * sensitivity;
+        CheckForWaterHeight();
 
-		//rotX = Input.GetKey (KeyCode.Joystick1Button4);
-		//rotY = Input.GetKey (KeyCode.Joystick1Button5);
+        Vector3 movement = new Vector3(moveFB, gravity, moveLR);
 
-		CheckForWaterHeight ();
+        if (webGLRightClickRotation)
+        {
+            if (Input.GetKey(KeyCode.Mouse0))
+            {
+                CameraRotation(cam, rotX, rotY);
+            }
+        }
+        else if (!webGLRightClickRotation)
+        {
+            CameraRotation(cam, rotX, rotY);
+        }
 
-		Vector3 movement = new Vector3 (moveFB, gravity, moveLR);
+        movement = transform.rotation * movement;
+        character.Move(movement * Time.deltaTime);
+    }
 
-		if (webGLRightClickRotation) {
-			if (Input.GetKey (KeyCode.Mouse0)) {
-				CameraRotation (cam, rotX, rotY);
-			}
-		} else if (!webGLRightClickRotation) {
-			CameraRotation (cam, rotX, rotY);
-		}
+    void CameraRotation(GameObject cam, float rotX, float rotY)
+    {
+        // Horizontal rotation (Y axis)
+        transform.Rotate(0, rotX * Time.deltaTime, 0);
 
-		movement = transform.rotation * movement;
-		character.Move (movement * Time.deltaTime);
-	}
+        // Vertical rotation (X axis) with clamping
+        xRotation -= rotY * Time.deltaTime; // Subtract to invert vertical look
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f); // Limit rotation between -90 and 90 degrees
 
-	void CameraRotation(GameObject cam, float rotX, float rotY)
-	{		
-		transform.Rotate (0, rotX * Time.deltaTime, 0);
-		cam.transform.Rotate (-rotY * Time.deltaTime, 0, 0);
-	}
-
+        // Apply clamped rotation to the camera
+        cam.transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+    }
 }
